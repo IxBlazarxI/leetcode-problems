2754f651d27d5d7978534e2ca600aefaf1defcea
IxBlazarxI <blazar11111@gmail.com>
2025-08-15 17:13:44 +0530

    Problem 342 added

---
+++ Easy Problems/342. Power of Four.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n <= 0) {
+        return false;
+    }
+    let x = Math.log(n) / Math.log(4);
+    if (Number.isInteger(x)) {
+        return true;
+    }
+    return false;
+};
