b1bb5efc777c652e16a0fcf5e75b2e5ea8acdd9e
IxBlazarxI <blazar11111@gmail.com>
2025-08-29 12:59:32 +0530

    Added problem 3021

--- Easy Problems/14. Longest Common Prefix.js
+++ Easy Problems/14. Longest Common Prefix.js
@@ -5,7 +5,19 @@
 let strs = ["flower", "flow", "flight"];
 var longestCommonPrefix = function(strs) {
     res = "";
-    for (i = 0; i < strs[0].length; i++) {
-             
+    if (strs.length <= 1) {
+        return strs[0];
+    } else {
+        for (let i = 0; i < strs[0].length; i++) {
+            for (let j = 1; j < strs.length; j++) {
+                if (strs[0][i] !== strs[j][i]) {
+                    return res;
+                }
+            }
+            res += strs[0][i];
+        }
     }
+    return res;
 };
+
+console.log(longestCommonPrefix(strs));
