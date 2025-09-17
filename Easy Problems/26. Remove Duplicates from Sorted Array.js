24aeb3f0fa85167a39ab26ee3454aa18a205d0c6
IxBlazarxI <blazar11111@gmail.com>
2025-09-17 19:15:53 +0530

    added a few more problems

---
+++ Easy Problems/26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+};
+
+console.log(removeDuplicates([1, 1, 2, 3, 4, 4, 4]));
