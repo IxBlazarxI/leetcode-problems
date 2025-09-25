15437fe4b4511dd49375473012de30ba28c0fa7f
IxBlazarxI <blazar11111@gmail.com>
2025-09-25 12:48:26 +0530

    added problem 120, my first DP problem

---
+++ Medium Problems/120. Triangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+  for (let i = 1; i < triangle.length; i++) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      if (j === 0) {
+        triangle[i][j] += triangle[i - 1][j];
+      } else if (j === triangle[i].length - 1) {
+        triangle[i][j] += triangle[i - 1][j - 1];
+      } else {
+        triangle[i][j] += Math.min(triangle[i - 1][j], triangle[i - 1][j - 1]);
+      }
+    }
+  }
+  return Math.min(...triangle[triangle.length - 1]);
+};
+
+let trgle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]];
+console.log(minimumTotal(trgle));
