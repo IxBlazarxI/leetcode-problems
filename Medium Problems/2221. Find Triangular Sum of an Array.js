a9deb9dd22346aa6ad9971f1e486437dd439abf6
IxBlazarxI <blazar11111@gmail.com>
2025-09-30 12:49:35 +0530

    added problem 2221

---
+++ Medium Problems/2221. Find Triangular Sum of an Array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function(nums) {
+  if (nums.length == 1) {
+    return nums[0];
+  }
+
+  let newNums = [];
+  while (nums.length > 1) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      newNums.push((nums[i] + nums[i + 1]) % 10);
+    }
+    nums = newNums;
+    newNums = [];
+  }
+  return nums[0];
+};
+
+let someArr = [1,2,3,4,5];
+console.log(triangularSum(someArr));
